2d7cc7b1a5100acc05bfc6d3f9245d9467b7d0db
Dario Cangialosi <dario.cangialosi@gmail.com>
2025-02-20 21:32:13 +0100

    Enhance todo manager with detailed PHPDoc comments and improve input validation

--- orm-redbean/todo-manager.php
+++ orm-redbean/todo-manager.php
@@ -2,48 +2,87 @@
 
 require_once 'red-bean-orm-use.php';
 
-//-----------------------------------------
-// todo-manager.php
-//-----------------------------------------
-
-function todo_list()
+/**
+ * Retrieves all todos from the database.
+ *
+ * @return array An array of todo objects.
+ */
+function todo_list(): array
 {
     return R::findAll('todos');
 }
 
-function todo_remove(int $id)
+/**
+ * Removes a todo by its ID.
+ *
+ * @param int $id The ID of the todo to remove.
+ * @return bool Returns true if the todo was found and removed; false otherwise.
+ */
+function todo_remove(int $id): bool
 {
     $todo = R::load('todos', $id);
+    if (! $todo || $todo->id === 0) {
+        return false;
+    }
     R::trash($todo);
+    return true;
 }
 
-function todo_add(string $description)
+/**
+ * Adds a new todo item.
+ *
+ * @param string $description The description of the todo.
+ * @return int|null Returns the new todo's ID if added; null if the description is empty.
+ */
+function todo_add(string $description): ?int
 {
     $description = trim($description);
-    if ($description == '') {
-        return;
+    if ($description === '') {
+        return null;
     }
-
     $todo              = R::dispense('todos');
     $todo->description = $description;
     $todo->state       = 0;
-    R::store($todo);
+    return R::store($todo);
 }
 
-function todo_update_description(int $id, string $description)
+/**
+ * Updates the description of a todo item.
+ * If the new description is empty after trimming, the todo will be removed.
+ *
+ * @param int $id The ID of the todo to update.
+ * @param string $description The new description for the todo.
+ * @return bool Returns true if the update or removal was successful; false otherwise.
+ */
+function todo_update_description(int $id, string $description): bool
 {
-    if (trim($description) == '') {
-        todo_remove($id);
-        return;
+    $trimmedDesc = trim($description);
+    if ($trimmedDesc === '') {
+        return todo_remove($id);
     }
-    $todo              = R::load('todos', $id);
-    $todo->description = $description;
+    $todo = R::load('todos', $id);
+    if (! $todo || $todo->id === 0) {
+        return false;
+    }
+    $todo->description = $trimmedDesc;
     R::store($todo);
+    return true;
 }
 
-function todo_update_state(int $id, int $state)
+/**
+ * Updates the state of a todo item.
+ *
+ * @param int $id The ID of the todo.
+ * @param int $state The new state for the todo.
+ * @return bool Returns true if the state was updated successfully; false otherwise.
+ */
+function todo_update_state(int $id, int $state): bool
 {
-    $todo        = R::load('todos', $id);
+    $todo = R::load('todos', $id);
+    if (! $todo || $todo->id === 0) {
+        return false;
+    }
     $todo->state = $state;
     R::store($todo);
+    return true;
 }
